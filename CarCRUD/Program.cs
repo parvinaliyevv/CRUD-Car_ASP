e4c82a2a6ad5622550d35e63ca44f4a6944caa98
parvinaliyevv <aliyevv_p@outlook.com>
2022-11-14 00:57:32 +0400

    change route

--- CarCRUD/Program.cs
+++ CarCRUD/Program.cs
@@ -7,6 +7,36 @@ var app = builder.Build();
 app.UseStaticFiles();
 
 app.MapControllerRoute(
+    name: "default",
+    pattern: "Home",
+    defaults: new { controller = "Car", action = "Home" }
+);
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "Create",
+    defaults: new { controller = "Car", action = "Create" }
+);
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "Read",
+    defaults: new { controller = "Car", action = "Read" }
+);
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "Update",
+    defaults: new { controller = "Car", action = "Update" }
+);
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "Delete",
+    defaults: new { controller = "Car", action = "Delete" }
+);
+
+app.MapControllerRoute(
     name:"default",
     pattern:"{controller=Car}/{action=Home}/{id?}"
 );
