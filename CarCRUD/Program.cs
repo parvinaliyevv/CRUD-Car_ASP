67b72276f9c8fc69a110511b98eef78e5dbde435
parvinaliyevv <aliyevv_p@outlook.com>
2022-11-14 15:51:46 +0400

    add route

--- CarCRUD/Program.cs
+++ CarCRUD/Program.cs
@@ -8,32 +8,8 @@ app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "Home",
-    defaults: new { controller = "Car", action = "Home" }
-);
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "Create",
-    defaults: new { controller = "Car", action = "Create" }
-);
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "Read",
-    defaults: new { controller = "Car", action = "Read" }
-);
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "Update",
-    defaults: new { controller = "Car", action = "Update" }
-);
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "Delete",
-    defaults: new { controller = "Car", action = "Delete" }
+    pattern: "{action}",
+    defaults: new { controller = "Car" }
 );
 
 app.MapControllerRoute(
